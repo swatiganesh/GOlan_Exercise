310e5f187a8017ef487bc67cea24be2c98a4cd1b
swatign <SwatiGanesh.Naik@Progress.com>
2023-09-22 09:25:36 +0530

    variables

---
+++ golanpractice/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	//fmt.Println("HI Everyone")
+
+	var fruit = "banana"
+	const quantity = 10
+
+	fmt.Println("I like", fruit, "so I bought", quantity, fruit, "\n")
+
+	var fname string = "Swati"
+	var lname string = "Naik"
+
+	fmt.Printf("my sweet name is %v %v and everyone likes me ", fname, lname)
+
+	fmt.Printf("")
+}
