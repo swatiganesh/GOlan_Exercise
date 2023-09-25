7c96e35c98b2da7d71d4d97d7fb5e90c25fa5f95
swatign <SwatiGanesh.Naik@Progress.com>
2023-09-25 13:57:04 +0530

    structure

--- practise8/ecp7.go
+++ practise8/ecp7.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
+type Student struct {
+	rollno int
+	fname  string
+	lname  string
+	marks  float32
+}
+
 func main() {
+	var s1 = Student{
+		100,
+		"Swati",
+		"Naik",
+		45.5,
+	}
+	fmt.Println(s1)
 
 }
+
+//https://github.com/swatiganesh/all-examples.git
