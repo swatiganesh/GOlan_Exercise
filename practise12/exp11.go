721545d3ab2e66a38b22c92d7a54fe88a08c8684
swatign <SwatiGanesh.Naik@Progress.com>
2023-09-25 16:44:33 +0530

    maps in go

--- practise12/exp11.go
+++ practise12/exp11.go
@@ -17,8 +17,6 @@ func main() {
 	delete(languages, "RB")
 	fmt.Println("List of all languages: ", languages)
 
-	// loops are interesting in golang
-
 	for _, value := range languages {
 		fmt.Printf("For key v, value is %v\n", value)
 	}
